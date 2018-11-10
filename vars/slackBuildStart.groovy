48f38d4b498a1e414c32215cc6cb776a2774c8d0
EZ Bardeguez <ez@eztheog.com>
2018-11-09 23:23:00 -0500

    comma

--- vars/slackBuildStart.groovy
+++ vars/slackBuildStart.groovy
@@ -1,6 +1,6 @@
 def call(String channel) {
 	slackSend message: "Started <${env.JOB_URL}|${env.JOB_NAME}>|(<${env.BUILD_URL}|#${env.BUILD_NUMBER}>)",
-		attachment: 'https://butt.holdings/hold-onto-your-butts.gif'
+		attachment: 'https://butt.holdings/hold-onto-your-butts.gif',
 		channel: "${channel}",
 		color: "#764FA5"
 }
