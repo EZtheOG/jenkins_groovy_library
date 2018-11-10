21e6a2544ac9cc34eda826f6cddd85947061e23d
EZ Bardeguez <ez@eztheog.com>
2018-11-09 23:22:03 -0500

    adding color and trying attachment

--- vars/slackBuildStart.groovy
+++ vars/slackBuildStart.groovy
@@ -1,8 +1,8 @@
 def call(String channel) {
-	slackSend message: "Started <${env.JOB_URL}. \
-		|${env.JOB_NAME}> (<${env.BUILD_URL}|#${env.BUILD_NUMBER}>)\n \
-		<https://butt.holdings>",
-		channel: "${channel}"
+	slackSend message: "Started <${env.JOB_URL}|${env.JOB_NAME}>|(<${env.BUILD_URL}|#${env.BUILD_NUMBER}>)",
+		attachment: 'https://butt.holdings/hold-onto-your-butts.gif'
+		channel: "${channel}",
+		color: "#764FA5"
 }
 
 def call(channel) {
